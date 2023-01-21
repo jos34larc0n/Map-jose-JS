e2acd29296b9e50ac78b328f7b7b37f9d49f0246
JOSE ALARCON MOLINA <jos3.4larc0n@gmail.com>
2023-01-21 14:41:14 -0500

    improved

--- app.js
+++ app.js
@@ -1,46 +1,63 @@
+let map
+let coordinates = []
+let businesses = []
+let markers = {}
+
 /* Obtain the user's current location. */
 function getLocation() {
-    if (navigator.geolocation) {
-      navigator.geolocation.getCurrentPosition(function (position) {
-        const coordinates = [
-          position.coords.latitude,
-          position.coords.longitude
-        ];
-        const map = buildMap(coordinates);
-        const marker = L.marker(this.coordinates)
-        marker
-        .addTo(this.map)
-        .bindPopup('<p1><b>You are here</b><br></p1>')
-        .openPopup()
-      });
-    } else {
-      alert('Geolocation is not supported by this browser.');
-    }
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(function(position) {
+      const coordinates = [
+        position.coords.latitude,
+        position.coords.longitude
+      ];
+      buildMap(coordinates);
+      const marker = L.marker(coordinates)
+      marker
+      .addTo(map)
+      .bindPopup('<p1><b>You are here</b><br></p1>')
+      .openPopup()
+    }, () => console.log('Geolocation error'));
+  } else {
+    alert('Geolocation is not supported by this browser.');
   }
-getLocation()
-/*getCoords() function to get coordinates */
-/*async function getCoords(){
-	const pos = await new Promise((resolve, reject) => {
-		navigator.geolocation.getCurrentPosition(resolve, reject)
-	});
-	return [pos.coords.latitude, pos.coords.longitude]
-}*/
-/* Map the location on a Leaflet map.*/
-const map = L.map('map').setView([35.884766, -78.625053], 13);
-L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
-    attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-}).addTo(map);
+}
 /*Create leaflet map */
 function buildMap(coordinates) {
-    const map = L.map('map').setView(coordinates, 13);
-    /*L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}'. Change to streetmap*/
+    map = L.map('map').setView(coordinates, 13);
+
     L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
 	  attribution:'&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
 	  minZoom: '15',
     }).addTo(map);
-    return map;
   }
 
-/* Allow the user to select a business type from a list and map the five nearest locations on the map using the Foursquare API. */
+getLocation()
+/*// get foursquare request function*/
+async function getFoursquare(business) {
+	const options = {
+    method: 'GET',
+    headers: {
+      accept: 'application/json',
+      Authorization: 'fsq3+HNcRJZVhPsjOls2sGNi3bWn5ldhGm+iTWdwDVMdKCI='
+    }
+  }
+	let limit = 5
+	let lat = map.coordinates[0]
+	let lon = map.coordinates[1]
+	let response = await fetch(`https://api.foursquare.com/v3/places/search?&query=${business}&limit=${limit}&ll=${lat}%2C${lon}`, options)
+	let data = await response.text()
+	let parsedData = JSON.parse(data)
+	let businesses = parsedData.results
+	return businesses
+}
+/*add event listeners */
 
+/*document.getElementById('submit').addEventListener('click', async (event) => {
+	event.preventDefault()
+	let business = document.getElementById('business').value
+	let data = await getFoursquare(business)
+	map.businesses = processBusinesses(data)
+	map.addMarkers()
+})*/
 
